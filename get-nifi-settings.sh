3ab5d7ff5906d545dec50520493d700002a90fc6
Ed Outhwaite <Ed.Outhwaite@MarkLogic.com>
2018-12-14 12:55:55 +0000

    update to 1.8

--- get-nifi-settings.sh
+++ get-nifi-settings.sh
@@ -12,18 +12,18 @@ echo .
 echo press ctrl+c to cancel or enter to continue
 read input
 
-/opt/nifi-1.6.0/bin/nifi.sh stop
+/opt/nifi-1.8.0/bin/nifi.sh stop
 
-cp /opt/nifi-1.6.0/conf/custom.properties settings/conf/$ENV.custom.properties
-cp /opt/nifi-1.6.0/conf/system.properties settings/conf/system.properties
+cp /opt/nifi-1.8.0/conf/custom.properties settings/conf/$ENV.custom.properties
+cp /opt/nifi-1.8.0/conf/system.properties settings/conf/system.properties
 
 # --keep is not an option on CentOS...
 rm settings/conf/flow.xml
-cp /opt/nifi-1.6.0/conf/flow.xml.gz settings/conf/flow.xml.gz.a
-cp /opt/nifi-1.6.0/conf/flow.xml.gz settings/conf/flow.xml.gz
+cp /opt/nifi-1.8.0/conf/flow.xml.gz settings/conf/flow.xml.gz.a
+cp /opt/nifi-1.8.0/conf/flow.xml.gz settings/conf/flow.xml.gz
 gunzip settings/conf/flow.xml.gz
 mv settings/conf/flow.xml.gz.a settings/conf/flow.xml.gz
 
 echo now start nifi manually:
-echo /opt/nifi-1.6.0/bin/nifi.sh start
+echo /opt/nifi-1.8.0/bin/nifi.sh start
 echo .
--- start-nifi.sh
+++ start-nifi.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-/opt/nifi-1.6.0/bin/nifi.sh start
-tail -f /opt/nifi-1.6.0/logs/*[pr].log
-
+/opt/nifi-1.8.0/bin/nifi.sh start
+tail -f /opt/nifi-1.8.0/logs/*[pr].log
